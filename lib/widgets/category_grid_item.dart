d5f337b143f5db944cf9e9bf86299e70ff619dc2
Danish Khan <danish.khan@ebpearls.com>
2024-05-30 15:18:56 +0545

    first commit

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category, required this.onSelectCategory});
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    /// Both Inkwell and GestureDetector can be used for tappable where
+    /// Inkwell provices a nice visual feedback, if a user taps the item.
+    /// But with GestureDetector, you would get no feedback.
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration( 
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
+      ),
+    );
+  }
+}
